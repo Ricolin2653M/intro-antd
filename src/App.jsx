4f45514ebb2495c104ef184294bce3743f1001a2
Ricolin2653M <car6920.03.03@gmail.com>
2024-05-27 20:34:04 -0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import {  ConfigProvider } from 'antd'
+import { ConfigProvider } from 'antd'
 import './App.css'
 import LayoutComponent from './components/Layout/index.jsx'
+import FormLogin from './components/FormLogin/index.jsx';
+import ImageLogin from './components/img/imageLogin.jsx';
 
 function App() {
 
   return (
     <ConfigProvider
-    theme={{
-      token: {
-        colorPrimary: '#FD861D'
-      }
-    }}
+      theme={{
+        token: {
+          colorPrimary: '#FD861D'
+        }
+      }}
     >
-      
-      <LayoutComponent></LayoutComponent>
+
+      <LayoutComponent
+
+        leftColSize={{ xs: 0, sm: 0, md: 8, lg: 6 }}
+        rightColSize={{ xs: 24, sm: 24, md: 16, lg: 18 }}
+        leftContent={<ImageLogin />}
+        rigthContent={<FormLogin />}
+      />
     </ConfigProvider>
   )
 }
