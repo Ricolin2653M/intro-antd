a47f0116d1d314f379b437e87df835e764ac1de6
Ricolin2653M <car6920.03.03@gmail.com>
2024-05-29 22:05:32 -0600

    new: authentication for form register and login

--- src/components/FormRegister/index.jsx
+++ src/components/FormRegister/index.jsx
@@ -1,14 +1,53 @@
-import { Form, Input, Button, Card, message } from 'antd'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import { Form, Input, Button, Card } from 'antd'
 import { UserOutlined, LockOutlined } from '@ant-design/icons'
 import './FormRegister.css'
 
-const FormLogin = () => {
-    const onFinish = (values) => {
+const FormRegister = () => {
+    const navigate = useNavigate();
+
+    //Estado de error de registro
+    const [registerError, setRegisterError] = useState(false);
+    //Estado de carga
+    const [loading, setLoading] = useState(false);
+
+    const onFinish = async (values) => {
+        setLoading(true);
+        try {
+            const response = await axios.post('https://lizard-server.vercel.app/api/auth/signup', {
+                username: values.username,
+                email: values.email,
+                password: values.password,
+                roles: ['user']
+            });
+            console.log('Registro exitoso: ', response.data);
+            navigate('login');
+        } catch (error) {
+            if (error.response) {
+                console.error('Error en el regis:', error.response.data);
+            } else {
+                console.error('Error en el regis:', error.message);
+            }
+            setRegisterError(true);
+        } finally {
+            setLoading(false);
+        }
         console.log('Success: ', values);
     }
     const onFinishFailed = (errorInfo) => {
         console.log('Failed: ', errorInfo);
+        setRegisterError(true);
     }
+    const validatePassword = ({ getFieldValue }) => ({
+        validator(_, value) {
+            if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+            }
+            return Promise.reject(new Error('Las contraseñas no coinciden'));
+        },
+    });
     return (
         <>
             <Card
@@ -17,12 +56,13 @@ const FormLogin = () => {
                 className='responsive-card'
             >
                 <Form
-                    name="username"
-                    className="login-form"
+                    name="normal_register"
+                    className="register-form"
                     initialValues={{
                         remember: true,
                     }}
                     onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
                 >
                     <Form.Item
                         name="username"
@@ -31,7 +71,16 @@ const FormLogin = () => {
                             message: 'Por favor ingrese su usuario'
                         }]}
                     >
-                        <Input prefix={<UserOutlined />} placeholder='Usuario' />
+                        <Input prefix={<UserOutlined className='site-form-item-icon' />} placeholder='Usuario' />
+                    </Form.Item>
+                    <Form.Item
+                        name="email"
+                        rules={[{
+                            required: true,
+                            message: 'Por favor ingrese su email'
+                        }]}
+                    >
+                        <Input prefix={<LockOutlined className='site-form-item-icon' />} placeholder='Email' />
                     </Form.Item>
                     <Form.Item
                         name="password"
@@ -40,18 +89,30 @@ const FormLogin = () => {
                             message: 'Por favor ingrese su contraseña'
                         }]}
                     >
-                        <Input.Password prefix={<LockOutlined />} placeholder='Contraseña'/>
+                        <Input.Password prefix={<LockOutlined className='site-form-item-icon' />} placeholder='Contraseña' />
                     </Form.Item>
+                    <Form.Item
+                        name="password-repet"
+                        rules={[{
+                            required: true,
+                            message: 'Por favor repita su contraseña'
+                        },
+                        ({ getFieldValue }) => validatePassword({ getFieldValue }),
+                        ]}
+                    >
+                        <Input.Password prefix={<LockOutlined className='site-form-item-icon' />} placeholder='Contraseña' />
+                    </Form.Item>
+
                     <Form.Item>
-                        <Button type="primary" htmlType="submit" className="login-form-button">
-                            Iniciar sesión
+                        {registerError && <p style={{ color: 'red' }}>Falló el registro</p>}
+                        <Button type="primary" htmlType="submit" className="login-form-button" loading={loading}>
+                            Registrar
                         </Button>
                     </Form.Item>
-                    ¿Aún no tienes cuenta? <a href="">Registrate</a>
                 </Form>
             </Card>
         </>
     );
 }
 
-export default FormLogin;
+export default FormRegister;
