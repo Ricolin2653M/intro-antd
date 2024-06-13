f8829d25079963487b3db4ca6956badce61ebc14
danielpresa <dan.pres.mar@gmail.com>
2024-06-12 18:13:59 -0600

    Front:NavBar Creado

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,13 +1,15 @@
 import { Button } from "antd";
 import { useAuth } from "../../../hooks/useAuth";
+import Nav from '../../Nav/index';
 
 const Home = () => {
     const {user, logout} = useAuth();
     //console.log("Datos de usuario:\n", JSON.stringify(user));
     return (
         <div>
+            <Nav></Nav>
             <h1>Hola, {user.readerFound.readername}</h1>
-            <Button onClick={()=>logout()}>Cerrar sesion</Button>
+            
         </div>
 
     );
