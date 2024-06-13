d8cb8d052d06ccb35565c25dd3a46f597020967d
danielpresa <dan.pres.mar@gmail.com>
2024-06-13 17:31:55 -0600

    fetch:actualizate Nav

--- src/components/Drawer/index.jsx
+++ src/components/Drawer/index.jsx
@@ -3,6 +3,7 @@ import { Drawer, Avatar, Button } from 'antd';
 import { UserOutlined, MailOutlined, LogoutOutlined } from '@ant-design/icons';
 import { useAuth } from '../../hooks/useAuth';
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 const DrawerComponent = () => {
     const { user, logout } = useAuth();
@@ -17,30 +18,40 @@ const DrawerComponent = () => {
     };
 
     return (
-        <>
-            <Avatar
-                onClick={showDrawer}
-                size={44}
-                style={{ backgroundColor: '#87d068', cursor: 'pointer' }}
-                icon={<UserOutlined />}
-            />
-            <Drawer title="Perfil" onClose={onClose} open={open}>
-                <div className="drawer-content">
-                    <div className="info-section">
-                        <UserOutlined className="info-icon" />
-                        <p className="info-label">Usuario:</p>
-                        <p className="info-value">{user.readerFound.readername}</p>
-                    </div>
-                    <div className="info-section">
-                        <MailOutlined className="info-icon" />
-                        <p className="info-label">Correo:</p>
-                        <p className="info-value">{user.readerFound.email}</p>
-                    </div>
-                    <Button className="logout-button" onClick={logout} icon={<LogoutOutlined />}>
-                        Cerrar sesión
-                    </Button>
-                </div>
-            </Drawer>
+        <>  
+            {user ? (
+                <>
+                    <Avatar
+                        onClick={showDrawer}
+                        size={44}
+                        style={{ backgroundColor: '#AFB3FF', cursor: 'pointer' }}
+                        icon={<UserOutlined />}
+                    />
+                    <Drawer title="Perfil" onClose={onClose} open={open}>
+                        <div className="drawer-content">
+                            <div className="info-section">
+                                <UserOutlined className="info-icon" />
+                                <p className="info-label">Usuario:</p>
+                                <p className="info-value">{user.readerFound.readername}</p>
+                            </div>
+                            <div className="info-section">
+                                <MailOutlined className="info-icon" />
+                                <p className="info-label">Correo:</p>
+                                <p className="info-value">{user.readerFound.email}</p>
+                            </div>
+                            <Button className="logout-button" onClick={logout} icon={<LogoutOutlined />}>
+                                Cerrar sesión
+                            </Button>
+                        </div>
+                    </Drawer>
+                </>
+            ) : (
+                <Link to="/login">
+                    <Button type="primary">Iniciar Sesión</Button>
+                </Link>
+            )}
+
+            
         </>
     );
 };
