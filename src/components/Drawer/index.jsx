6b33084b80827922bc18f9e01911a3ee3cb5937e
cesargalvan <cesarspace10@gmail.com>
2024-06-24 17:29:01 -0600

    Nuevos cambios de interfaces

--- src/components/Drawer/index.jsx
+++ src/components/Drawer/index.jsx
@@ -1,20 +1,18 @@
-import { useState } from 'react';
-import { Drawer, Avatar, Button, } from 'antd';
+import React, { useState } from 'react';
+import { Drawer, Avatar, Button, Modal, Input, Form, notification } from 'antd';
 import { UserOutlined, MailOutlined, LogoutOutlined, LockOutlined } from '@ant-design/icons';
 import { useAuth } from '../../hooks/useAuth';
-import { Link, } from 'react-router-dom';
-import ModalPassword from '../Modals/ModalPassword';
-import ModalUsername from '../Modals/ModalUsername';
-import ModalEmail from '../Modals/ModalEmail';
 import './styles.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { usersService } from '../../services/users';
 
 const DrawerComponent = () => {
+    const navigate = useNavigate();
     const { user, logout } = useAuth();
     const [open, setOpen] = useState(false);
-    const [isModalPasswordOpen, setIsModalPasswordOpen] = useState(false);
-    const [isModalUsernameOpen, setIsModalUsernameOpen] = useState(false);
-    const [isModalEmailOpen, setIsModalEmailOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const [loading, setLoading] = useState(false);
+    const [form] = Form.useForm();
 
     const showDrawer = () => {
         setOpen(true);
@@ -24,28 +22,44 @@ const DrawerComponent = () => {
         setOpen(false);
     };
 
-    const showModalPassword = () => {
-        setIsModalPasswordOpen(true);
+    const showModal = () => {
+        setIsModalOpen(true);
     };
 
-    const showModalUsername = () => {
-        setIsModalUsernameOpen(true);
+    const handleCancel = () => {
+        form.resetFields();
+        setIsModalOpen(false);
     };
 
-    const showModalEmail = () => {
-        setIsModalEmailOpen(true);
-    };
+    const validatePassword = ({ getFieldValue }) => ({
+        validator(_, value) {
+            if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+            }
+            return Promise.reject(new Error('Las contraseñas no coinciden'));
+        },
+    });
 
-    const handleCancelPassword = () => {
-        setIsModalPasswordOpen(false);
-    };
-
-    const handleCancelUsername = () => {
-        setIsModalUsernameOpen(false);
-    };
-
-    const handleCancelEmail = () => {
-        setIsModalEmailOpen(false);
+    const onFinish = async (values) => {
+        setLoading(true);
+        try {
+            await usersService.updatePassword(user.readerFound._id, values.password);
+            notification.success({
+                message: 'Cambio exitoso',
+                description: 'La contraseña se ha actualizado correctamente.',
+            });
+            form.resetFields(); 
+            setIsModalOpen(false);
+            navigate('/');
+        } catch (error) {
+            console.error('Error al actualizar contraseña:', error);
+            notification.error({
+                message: 'Error al actualizar contraseña',
+                description: 'Hubo un problema al intentar actualizar la contraseña.',
+            });
+        } finally {
+            setLoading(false);
+        }
     };
 
     return (
@@ -61,52 +75,57 @@ const DrawerComponent = () => {
                     <Drawer title="Perfil" onClose={onClose} open={open}>
                         <div className="drawer-content">
                             <div className="info-section">
-                                <div className="info-content">
-                                    <UserOutlined className="info-icon" />
-                                    <p className="info-label">Usuario:</p>
-                                    <p className="info-value">{user.readerFound.readername}</p>
-                                </div>
-                                <Button className="edit-button-username" onClick={showModalUsername}>Editar usuario</Button>
+                                <UserOutlined className="info-icon" />
+                                <p className="info-label">Usuario:</p>
+                                <p className="info-value">{user.readerFound.readername}</p>
                             </div>
                             <div className="info-section">
-                                <div className="info-content">
-                                    <MailOutlined className="info-icon" />
-                                    <p className="info-label">Correo:</p>
-                                    <p className="info-value">{user.readerFound.email}</p>
-                                </div>
-                                <Button className="edit-button-email" onClick={showModalEmail}>Editar correo</Button>
+                                <MailOutlined className="info-icon" />
+                                <p className="info-label">Correo:</p>
+                                <p className="info-value">{user.readerFound.email}</p>
                             </div>
-                            <div className="info-section">
-                                <div className="info-content">
-                                    <LockOutlined className="info-icon" />
-                                    <p className="info-label">Contraseña:</p>
-                                    <p className="info-value">****</p>
-                                </div>
-                                <Button className="edit-button-password" onClick={showModalPassword}>Editar contraseña</Button>
-                            </div>
+                            <Button className="edit-button" onClick={showModal}>Editar contraseña</Button>
                             <Button className="logout-button" onClick={logout} icon={<LogoutOutlined />}>
-                                <strong>Cerrar sesión</strong>
+                                Cerrar sesión
                             </Button>
                         </div>
                     </Drawer>
-                    <ModalPassword
-                        isVisible={isModalPasswordOpen}
-                        onCancel={handleCancelPassword}
-                        loading={loading}
-                        setLoading={setLoading}
-                    />
-                    <ModalUsername
-                        isVisible={isModalUsernameOpen}
-                        onCancel={handleCancelUsername}
-                        loading={loading}
-                        setLoading={setLoading}
-                    />
-                    <ModalEmail
-                        isVisible={isModalEmailOpen}
-                        onCancel={handleCancelEmail}
-                        loading={loading}
-                        setLoading={setLoading}
-                    />
+                    <Modal
+                        title="Editar Contraseña"
+                        visible={isModalOpen}
+                        onOk={() => form.submit()}
+                        onCancel={handleCancel}
+                        confirmLoading={loading}
+                    >
+                        <div className="modal-content">
+                            <Form
+                                form={form}
+                                name="change_password"
+                                className="password-form"
+                                initialValues={{}}
+                                onFinish={onFinish}
+                            >
+                                <Form.Item
+                                    name="password"
+                                    rules={[{
+                                        required: true,
+                                        message: 'Por favor ingrese su contraseña'
+                                    }]}
+                                >
+                                    <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} placeholder="Contraseña" />
+                                </Form.Item>
+                                <Form.Item
+                                    name="password-repet"
+                                    rules={[{
+                                        required: true,
+                                        message: 'Por favor repita su contraseña'
+                                    }, ({ getFieldValue }) => validatePassword({ getFieldValue })]}
+                                >
+                                    <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} placeholder="Contraseña" />
+                                </Form.Item>
+                            </Form>
+                        </div>
+                    </Modal>
                 </>
             ) : (
                 <Link to="/login">
